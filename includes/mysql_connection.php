463a88af8af2bdc259a22a4c1d10dc4135e90c3b
Alex Shortt <alex@trestcreative.com>
2013-07-03 17:52:14 +0100

    Complete Files from Server

--- includes/mysql_connection.php
+++ includes/mysql_connection.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $conn = new PDO('mysql:host=localhost;dbname=forma', 'root', '');
+    $conn = new PDO('mysql:host=formafitnessgb.com.mysql;dbname=formafitnessgb_', 'formafitnessgb_', 'jHn4mbJY');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
